a9382f91c40632871540aa37b97aff1ed7a4014e
akilinjamam <akilinjamam@gmail.com>
2025-09-11 11:41:39 +0600

    api-updated

--- src/app/redux/api/baseApi.ts
+++ src/app/redux/api/baseApi.ts
@@ -3,6 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://eyeline-optica-server.onrender.com/api/v1/",
+  }),
   endpoints: () => ({}),
 });
