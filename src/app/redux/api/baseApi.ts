9f4f177d92a9a00d0bee004974733fac0ca5285e
akilinjamam <akilinjamam@gmail.com>
2026-02-16 18:23:47 +0600

    api-updated

--- src/app/redux/api/baseApi.ts
+++ src/app/redux/api/baseApi.ts
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://server.eyelineoptica.com/api/v1/",
+    baseUrl: "https://eyeline-optica-server-demo.onrender.com/api/v1/",
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as any).auth?.token;
       if (token) {
